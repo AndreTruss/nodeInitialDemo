b34b85d6c5009c907112b370b273cace9d87a930
AndreTruss <andrea.trussardi@gmail.com>
2022-09-20 17:14:16 +0200

    app js

--- server/app/app.js
+++ server/app/app.js
@@ -1,1 +1,38 @@
-console.log('Hello World');
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+const routes = require('./routes/auth-routes');
+const { connectDB } = require('./db');
+const { CORS_URL } = require('./config');
+
+const app = express();
+const httpServer = http.createServer( app );
+
+app.use( cors( {
+    origin: CORS_URL,
+    credentials: true,
+    optionsSuccessStatus: 200,
+  } ) );
+app.use( express.json() );
+//app.use(cookieParser());
+
+// Routes
+app.use( routes );
+
+// DB initialization
+connectDB();
+
+// Socket-io
+const io = new Server( httpServer, {
+  cors: {
+    origin: CORS_URL,
+  },
+});
+// Call the function on './socket-io', and pass io as argument
+require('./socket-io')(io);
+
+const PORT = process.env.PORT || 5000;
+httpServer.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
