694a33cce490d81fb1ab4ce8244d35810ea1c4bc
Andrea Trussardi <andrea.trussardi@gmail.com>
2022-09-22 10:01:15 +0200

    bcrypt pw

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -1,13 +1,33 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, 
         required: [ true, 'Please enter a name'] 
     },
   password: { type: String, 
-            required: [ true, 'Please enter a password'] },
-            minlength: [ 6, 'Password shold be at least 6 characters long']
+            required: [ true, 'Please enter a password'],
+            minlength: [ 6, 'Password shold be at least 6 characters long'],
+  },
 });
+
+userSchema.pre( 'save', async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.statics.userLogin = async function( name, password ) {
+  const user = await this.findOne({ name });
+  if( user ){
+    const comparePW = await bcrypt.compare( password, user.password );
+    if( comparePW ){
+      return user
+    }
+    throw Error('Wrong password')
+  }
+  throw Error("User doesn't exist")
+}
 
 const User = mongoose.model( 'user', userSchema );
 
